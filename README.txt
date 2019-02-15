4fcb0fdc1b41970743d765bc6d01f7e8e7a45910
KevinYean <keviny219559@gmail.com>
2019-02-15 00:59:26 -0800

    Prototype Finished
    Finished the crawler prototype to get all the salary of all the current nba players.
    Next step is to visualize the data.

--- README.txt
+++ README.txt
@@ -1,3 +1,8 @@
 Current Goal:
 
-Crawl through the rooster of a NBA team in basketball reference and crawl through every players and get their salaries over their career.
+Done-Crawl through the rooster of a NBA team in basketball reference and crawl through every players and get their salaries over their career.
+
+Crawl through the current NBA team in the 2018-2019 season to get every team.
+Classify within the text file each team or make seperate files for each
+
+Create 3 methods. One for the season, One for the team and one for the players
--- basketballReferenceScrapy/spiders/players_spider.py
+++ basketballReferenceScrapy/spiders/players_spider.py
@@ -4,55 +4,79 @@ import re
 #response.css('td[data-stat="player"]').getall() //Teams
 #response.css('td[data-stat="player"] a::attr(href)').getall()
 
+
 class QuotesSpider(scrapy.Spider):
     name = "player"
     DOWNLOAD_DELAY = 1
 
     def start_requests(self):
-        urls = [ #List of Urls to go through
-            "https://www.basketball-reference.com/teams/LAC/2019.html"
+        urls = [  # List of Urls to go through
+            "https://www.basketball-reference.com/leagues/NBA_2019.html", #2018-2019 Season
+            #"https://www.basketball-reference.com/teams/LAC/2019.html"
             #'https://www.basketball-reference.com/teams/PHI/2019.html',
             #"https://www.basketball-reference.com/players/e/embiijo01.html",
         ]
-        for url in urls: #Run parse for each url in urls
-            yield scrapy.Request(url=url, callback=self.parse)
+        for url in urls:  # Run parse for each url in urls
+            yield scrapy.Request(url=url, callback=self.parseLeague)
 
-    def parse(self, response):
-        urlString = str(response)
-
-        #Player specific
-        if "player" in urlString:
-            #Player's Name
-            playerName = response.css("h1::text").getall()
-            yield{
-            'Player': playerName
-            }
-            #Past Salary
-            salary = response.css("div#all_all_salaries").getall() #Get the comments sections which has information about salaries
-            listsalary = re.findall(r'<tr >(.+?)</tr>', str(salary)) #Regular expression to split with <td></td> and creates list
-            for i in listsalary[:-1]:
-                seasonSalary = re.findall(r' >(.+?)</td', i) #Regular expression to split with <tr></tr>
-                length = len(seasonSalary)
-                season = re.findall(r'>(.+?)</th>', i) #Regular expression to split with <td></td>
-                yield {
-                    'Season': season[0],
-                    'Salary': seasonSalary[length-1],
-                }
-            #Contract, current.
-            contract = response.xpath('//div[contains(@id,"all_contracts")]').get()
-            listContract = re.findall(r'class="">(.+?)</span>', str(contract)) #Regular expression to split with <td></td> and creates list
-            year = 2018
-            for i in listContract:
-                yield {
-                    'Season': str(year) + "-" + str(year+1)[2:],
-                    'Salary': i,
-                }
-                year+=1
-
-            
+    def parseLeague(self, response):
+        #League Specific
+        teams_page = response.css(
+            'th[data-stat="team_name"] a::attr(href)').getall()
+        nbaSeason = response.css(
+            'h1 span::text').get()
+        yield{
+            'NBA Season': str(nbaSeason)
+        }
+        if teams_page is not None:
+            for i in teams_page:
+                x = response.urljoin(i)
+                yield scrapy.Request(x, callback=self.parseTeam)
+    
+    def parseTeam(self,response):
         #Team Specific
-        next_page = response.css('td[data-stat="player"] a::attr(href)').getall()
+        next_page = response.css(
+            'td[data-stat="player"] a::attr(href)').getall()
+        nbaTeam = response.css(
+            'h1 span::text').getall()
+        yield{
+            'NBA Team': str(nbaTeam)
+        }
         if next_page is not None:
             for i in next_page:
                 x = response.urljoin(i)
-                yield scrapy.Request(x, callback=self.parse)
+                yield scrapy.Request(x, callback=self.parsePlayer)
+
+    def parsePlayer(self, response):
+        #Player's Name
+        playerName = response.css("h1::text").get()
+        playerTeam = response.xpath(
+            '//div[contains(@itemtype,"https://schema.org/Person")] //a[contains(@href,"teams")]')
+        playerTeam = playerTeam.css('a::text').get() #not clean
+        yield{
+            'Player': playerName,
+            'Team': playerTeam
+        }
+        #Past Salary
+        salary = response.css("div#all_all_salaries").getall()        # Get the comments sections which has information about salaries
+        listsalary = re.findall(r'<tr >(.+?)</tr>', str(salary))        # Regular expression to split with <td></td> and creates list
+        for i in listsalary[:-1]:
+            # Regular expression to split with <tr></tr>
+            seasonSalary = re.findall(r' >(.+?)</td', i)
+            length = len(seasonSalary)
+            # Regular expression to split with <td></td>
+            season = re.findall(r'>(.+?)</th>', i)
+            yield {
+                'Season': season[0],
+                'Salary': seasonSalary[length-1],
+            }
+        #Contract, current.
+        contract = response.xpath('//div[contains(@id,"all_contracts")]').get()
+        listContract = re.findall(r'class="">(.+?)</span>', str(contract))         # Regular expression to split with <td></td> and creates list
+        year = 2018
+        for i in listContract:
+            yield {
+                'Season': str(year) + "-" + str(year+1)[2:],
+                'Salary': i,
+            }
+            year += 1
